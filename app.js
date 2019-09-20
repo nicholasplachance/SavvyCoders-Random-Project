c83df27a5cc095007ae5911834edd12929f754e6
nick lachance <nicholasplachance@gmail.com>
2019-09-19 21:27:39 -0500

    Add recursion to greeter function

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const fakeButton = document.getElementById("fake-cta--button");
 function greeter() {
     const name = prompt("What is your name?");
     fakeButton.textContent = `Hello ${name}`;
+    if ( name === "") {
+        alert("You didn't give me a name!! Try again!")
+        greeter();
+    }
 }
 
 fakeButton.addEventListener("click", greeter);

aa69bb09162b817875a452ce278d8ba2d0c9338f
nick lachance <nicholasplachance@gmail.com>
2019-09-19 21:13:25 -0500

    Add greeter function in app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// querySelector will only grab the first occurrence of the element
 const fakeButton = document.getElementById("fake-cta--button");
 
 function greeter() {
-    let name = prompt("What is your name?");
-    alert(`Hello ${name}`);
+    const name = prompt("What is your name?");
+    fakeButton.textContent = `Hello ${name}`;
 }
 
 fakeButton.addEventListener("click", greeter);

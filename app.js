b3fe26ac876fb0b91a5addaed6c297c09584a551
nick lachance <nicholasplachance@gmail.com>
2019-09-24 20:47:21 -0500

    Add javascript function to open hamburger navigation on mobile

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // querySelector will only grab the first occurrence of the element
-const fakeButton = document.getElementById("fake-cta--button");
+const rootDiv = document.querySelector("#root");
+const fakeButton = document.querySelector("#fake-cta--button")
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+const navUl = document.querySelector("nav ul");
 
-function greeter() {
-    const name = prompt("What is your name?");
-    fakeButton.textContent = `Hello ${name}`;
-    if ( name === "") {
-        alert("You didn't give me a name!! Try again!")
-        greeter();
+function greeter(question = "What is your name") {
+    let answer = prompt(question);
+
+    if (answer === "") {
+        return greeter()
     }
+
+    return answer;
 }
 
-fakeButton.addEventListener("click", greeter);
+fakeButton.addEventListener("click", function() {
+    fakeButton.innerHTML = `Hello ${greeter()}!`;
+})
 
+hamburgerIcon.addEventListener("click", function() {
+    navUl.classList.toggle("is-hidden--mobile");
+})
+
+
+console.log(navUl);
